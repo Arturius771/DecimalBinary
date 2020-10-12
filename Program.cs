0686226b98c75e805a01f613967c6e3b8146441e
Artur Foden <FODENA@tcd.ie>
2020-10-12 22:33:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DecimalBinary
+{
+    class Program
+    {
+        int bin;
+        int dec;
+        int answer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press 1 to convert to binary, press 2 to convert to decimal");
+        }
+    }
+}
